28e3e381ca61f66f16b527dfcdf99dc5d8babfcd
dingdj <softtestddj@gmail.com>
2014-11-30 22:28:51 +0800

    add my profile

--- app/src/main/java/com/phy0312/shopassistant/activity/MyProfileFragment.java
+++ app/src/main/java/com/phy0312/shopassistant/activity/MyProfileFragment.java
@@ -1,12 +1,15 @@
 package com.phy0312.shopassistant.activity;
 
 
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.phy0312.shopassistant.R;
 
@@ -15,6 +18,29 @@ import com.phy0312.shopassistant.R;
  *
  */
 public class MyProfileFragment extends Fragment {
+
+    private RelativeLayout login_area_layout;
+    private TextView tv_myprofile_login;
+    private TextView tv_myprofile_desc;
+    private Button btn_myprofile_register;
+    private RelativeLayout login_data_layout;
+    private TextView tv_myprofile_name;
+    private ImageView iv_myprofile_grade;
+    private ImageView iv_right_arrow;
+    private TextView tv_myprofile_point;
+
+
+    private TextView tv_myprofile_ticket_package;
+    private TextView tv_myprofile_ticket;
+    private TextView tv_myprofile_order_manager;
+    private TextView tv_myprofile_points_detail;
+    private TextView tv_myprofile_member_card;
+
+    private TextView tv_myprofile_password_manager;
+    private TextView tv_myprofile_pay_password_manager;
+
+    private TextView tv_myprofile_wx_bind;
+    private TextView tv_myprofile_logout;
 
 
     public MyProfileFragment() {
