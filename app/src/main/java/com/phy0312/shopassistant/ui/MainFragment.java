a5a75ad8aca7d70d461b3acab951161d5b27e529
dingdj <softtestddj@gmail.com>
2014-12-05 18:16:21 +0800

    优化部分代码

--- app/src/main/java/com/phy0312/shopassistant/ui/MainFragment.java
+++ app/src/main/java/com/phy0312/shopassistant/ui/MainFragment.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -99,25 +98,6 @@ public class MainFragment extends Fragment implements PullToRefreshLayout.PullRe
         return view;
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-    }
 
     @Override
     public void onRefreshingUp() {
@@ -129,10 +109,6 @@ public class MainFragment extends Fragment implements PullToRefreshLayout.PullRe
         startLoad(false, true);
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
 
     /**
      * 开始加载数据
